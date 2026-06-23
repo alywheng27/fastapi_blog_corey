3556b8584615a82288757b7c48bdc974b7cf134a
Aly Mama <52185940+alywheng27@users.noreply.github.com>
2026-06-23 14:39:18 +0800

    Jinja2 Template

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI, Request
-# from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+# from fastapi.responses import HTMLResponse
+
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 posts: list[dict] = [
     {
@@ -32,12 +36,18 @@ posts: list[dict] = [
 # include_in_schema=False hides the route from the docs
 
 
-@app.get("/", response_class=HTMLResponse, include_in_schema=False)
-@app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
-def home():
-    return f"<h1>{posts[0]['title']}</h1>"
+# @app.get("/", response_class=HTMLResponse, include_in_schema=False)
+# @app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
+# def home():
+#     return f"<h1>{posts[0]['title']}</h1>"
 
 
-@app.get("/api/posts", response_class=HTMLResponse)
+@app.get("/", include_in_schema=False, name="home")
+@app.get("/posts", include_in_schema=False, name="posts")
+def home(request: Request):
+    return templates.TemplateResponse(request, "home.html", {"posts": posts, "title": "Home"})
+
+
+@app.get("/api/posts")
 def get_posts():
     return posts
